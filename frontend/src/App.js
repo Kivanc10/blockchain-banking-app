449b12c566d831ed8f7d3860a77c5fee147cd1fb
ArdaSonmezler <64904720+ArdaSonmezler@users.noreply.github.com>
2022-08-17 14:42:46 +0300

    adminLogin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import React from 'react'
 import 'antd/dist/antd.css';
 import Formm from "./components/formm";
 import Landing from "./components/landing";
+import Dashboard from './components/dashboard';
+import AdminLogin from './components/adminLogin'
 
 
 
 function App() {   
   return (  
     <div>
-      <Landing/>
+      <AdminLogin/>
     </div>
   );
 }
---
+++ frontend/src/components/dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+import { Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+
+export default class dashboard extends Component {
+  render() {
+    return (
+      <div style={{backgroundImage:'none'}}>TEST</div>
+    )
+  }
+}
