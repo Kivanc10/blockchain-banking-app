e2f6e8c5a38acad5ecdf0cde37b752e1d2e7de80
ArdaSonmezler <64904720+ArdaSonmezler@users.noreply.github.com>
2022-08-17 10:59:25 +0300

    landing page

--- frontend/src/components/landing.js
+++ frontend/src/components/landing.js
@@ -1,5 +1,7 @@
 import logo from './raw/logo.png';
 import './landing.css';
+import icerik1 from './raw/1.jpeg';
+import icerik2 from './raw/2.jpeg';
 import 'bootstrap/dist/css/bootstrap.css';
 import Container from 'react-bootstrap/Container';
 import { Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
@@ -7,13 +9,53 @@ import chart from './raw/Charts Section.png';
 import Button from 'react-bootstrap/Button';
 import metamask from './raw/metamask-icon.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMoneyBillTransfer, faChartSimple, faBitcoinSign, faBuildingColumns, faPersonBreastfeeding, faHandHoldingDollar} from '@fortawesome/free-solid-svg-icons'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import AbdullahK from './raw/AbdullahKusgulu.png';
+import ArdaS from './raw/ArdaSonmezler.png';
+import aysenB from './raw/aysenBakis.png';
+import BerkayT from './raw/BerkayTugut.png';
+import GizemZ from './raw/GizemZorba.png';
+import HuseyinO from './raw/HuseyinOzkale.png';
+import KaanK from './raw/kaanKaftanoglu.png';
+import KivancK from './raw/KvancAydogmus.png';
+import MusaM from './raw/MusaMeric.png';
+import senaN from './raw/senanur.png';
+import SimgeB from './raw/simgeBatur.png';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+import SinejanY from './raw/sinejanYildirim.png';
+import { faMoneyBillTransfer, faChartSimple, faBitcoinSign, faBuildingColumns, faPersonBreastfeeding, faHandHoldingDollar } from '@fortawesome/free-solid-svg-icons'
 <link href="//db.onlinewebfonts.com/c/9dc59b82721662bb26b8afb510dc29c8?family=Arima+Madurai" rel="stylesheet" type="text/css" />
 
-
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
 function App() {
   return (
-    
+
+
     <div className='body'>
 
       {/* Navbar  Start*/}
@@ -50,7 +92,7 @@ function App() {
 
       {/* Navbar End */}
       {/* Mini Title Start */}
-      <div className='container-fluid d-flex justify-content-center' style={{ marginTop: '150px' }}>
+      <div className='container-fluid d-flex justify-content-center' style={{ marginTop: '6em' }}>
         <div className='justify-content-center' style={{ fontSize: '1.4em' }}>
           Blockchain investment app for financier
         </div>
@@ -155,19 +197,323 @@ function App() {
       </div>
       {/* Services Section End */}
       {/* News Section Start */}
-      <div class="row justify-content-center text-center container-fluid" style={{ marginTop:'75px' }}>
+      <div class="row justify-content-center text-center container-fluid" style={{ marginTop: '75px' }}>
         <div class="col-8">
           <h1 style={{ color: 'blue' }}>Agenda & News</h1>
         </div>
       </div>
-      <div class="row justify-content-center text-center container-fluid">
-        <div class="col-6">
-          <p style={{ color: 'white', fontWeight: 'bold' }}> Inheritium is a revolutionary creation created to help you manage your money, save money and leave a legacy for future generations. If you want to leave a wonderful legacy to your children, we are here for you.
-          </p>
+      <div class="row justify-content-center text-center container-fluid" style={{ marginTop: '15px' }}>
+        <div class="col-12">
+          <Carousel responsive={responsive}>
+            <div>
+
+              <img className='img-fluid newsP'
+                src={icerik1}
+                alt='icerik'></img>
+
+            </div>
+            <div>
+
+              <img className='img-fluid newsP'
+                src={icerik2}
+                alt='icerik'></img>
+
+            </div>
+            <div>
+
+              <img className='img-fluid newsP'
+                src={icerik1}
+                alt='icerik'></img>
+
+            </div>
+            <div>
+
+              <img className='img-fluid newsP'
+                src={icerik2}
+                alt='icerik'></img>
+
+            </div>
+          </Carousel>
         </div>
       </div>
       {/* News Section End */}
+      {/* Teams Section Start */}
+      <div class="row justify-content-center text-center container-fluid">
+        <div class="col-12" style={{ marginTop: '75px' }}>
+          <h1 style={{ color: 'blue' }}>Our Teams</h1>
+        </div>
+        <div class="col-2">
+          <div class="card TeamsBorder" style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={senaN}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader">Senanur Katipoğlu</h6>
+              <p class="card-text">Mentor</p>
+              <p class="card-text">Product Owner</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card TeamsBorder " style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={SimgeB}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader">Simge Batur</h6>
+              <p class="card-text">Business Analysis</p>
+              <p class="card-text">Scrum Master</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card TeamsBorder" style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={aysenB}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader">Ayşen Bakış</h6>
+              <p class="card-text">Mentor</p>
+              <p class="card-text">Agile Coach</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card TeamsBorder" style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={ArdaS}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader">Arda Sönmezler</h6>
+              <p class="card-text">Software Developer</p>
+              <p class="card-text">Developer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+      <div class="row justify-content-center text-center container-fluid">
+        <div class="col-12" style={{ marginTop: '25px' }}>
+         
+        </div>
+     
+        <div class="col-2">
+          <div class="card TeamsBorder" style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={KaanK}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader">Kaan Kaftanoğlu</h6>
+              <p class="card-text">Business Analysis</p>
+              <p class="card-text">Developer</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card TeamsBorder" style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={aysenB}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader">Sinejan Yıldırım</h6>
+              <p class="card-text">Business Analysis</p>
+              <p class="card-text">Developer</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card TeamsBorder" style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={KivancK}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader">Kıvanç Aydoğmuş</h6>
+              <p class="card-text">Software Developer</p>
+              <p class="card-text">Developer</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card TeamsBorder" style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={GizemZ}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader">Gizem Zorba</h6>
+              <p class="card-text">Software Developer</p>
+              <p class="card-text">Developer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row justify-content-center align-self-center text-center container-fluid">
+        <div class="col-12 " style={{ marginTop: '25px' }}>
+         
+        </div>
+        <div class="col-2 ">
+          <div class="card TeamsBorder" style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={BerkayT}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader">Berkay Tuğut</h6>
+              <p class="card-text">Software Developer</p>
+              <p class="card-text">Developer</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card TeamsBorder" style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={AbdullahK}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader">Abdullah Küsgülü</h6>
+              <p class="card-text">Software Developer</p>
+              <p class="card-text">Developer</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card TeamsBorder" style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={HuseyinO}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader">Hüseyin Özkale</h6>
+              <p class="card-text">Software Developer</p>
+              <p class="card-text">Developer</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2 ">
+          <div class="card TeamsBorder" style={{ width: '18rem;' }}>
+          <img className='card-img-top'
+                src={MusaM}
+                alt='icerik'></img>
+            <div class="card-body">
+              <h6 class="card-title mb-2 teamsHeader ">Musa Meriç</h6>
+              <p class="card-text">Software Developer</p>
+              <p class="card-text">Developer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Teams Section End */}
 
+      {/*FAQ Section Start */}
+      <div class="row justify-content-center text-center container-fluid" style={{marginTop:'75px'}}>
+        <div class="col-8">
+          <h1 style={{ color: 'purple' }}>Frequently <span style={{ color: 'blue' }}>asked question</span></h1>
+        </div>
+      </div>
+      <div class="row justify-content-center text-center container-fluid">
+        <div class="col-6">
+          <p style={{ color: 'white', fontWeight: 'bold' }}>If you are not sure whether Draft is suitalbe for you or
+not, do not worry. We are here to explain everything you
+might want to know</p>
+        </div>
+      </div>
+      <div className='row justify-content-center text-center container-fluid'>
+        <div className='col-6 text-center'>
+      <Accordion className='accordion'>
+            <AccordionItem className='accordion-item'>
+                <AccordionItemHeading className='accordion-header'>
+                    <AccordionItemButton className='accordion-button'>
+                    What is the difference between INHERITIUM and a Bank account ? 
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel className='accordion-body'>
+                    <p>
+                    you can easily invest your accumulation via Inheritium
+on the contrary your bank account 
+                    </p>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem className='accordion-item'>
+                <AccordionItemHeading className='accordion-header'>
+                    <AccordionItemButton className='accordion-button'>
+                    Who can open INHERITIUM account ?
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel className='accordion-body'>
+                    <p>
+                    Anyone who is over the age of 18 and has no problems with legal processes can open an account in our system.
+                    </p>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem className='accordion-item'>
+                <AccordionItemHeading className='accordion-header'>
+                    <AccordionItemButton className='accordion-button'>
+                        
+Can my children transact through their accounts?
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel className='accordion-body'>
+                    <p>
+                    Children will not be able to perform transactions until they reach the legal age limit. The legal age has been determined as 18.
+                    </p>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem className='accordion-item'>
+                <AccordionItemHeading className='accordion-header'>
+                    <AccordionItemButton className='accordion-button'>
+                    Can I cancel my subscription or switch to another account anytime?
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel className='accordion-body'>
+                    <p>
+                    Our system has been designed to allow this easily.
+ You can cancel your subscription or switch to your 
+another Inheritium account.
+                    </p>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem className='accordion-item'>
+                <AccordionItemHeading className='accordion-header'>
+                    <AccordionItemButton className='accordion-button'>
+                    What happens if I invest less than X coins?
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel className='accordion-body'>
+                    <p>
+The minimum order amount is set as 10 TRY by our system.
+                    </p>
+                </AccordionItemPanel>
+            </AccordionItem>
+        </Accordion>
+        </div>
+        </div>
+      {/*FAQ Section End */}
+      <div class="container-fluid bg-light">
+  <footer class="row row-cols-5 py-5 my-5 border-top">
+    <div class="col-8 text-center">
+      <h6>Contact :</h6>
+      <p>
+        Sanayi Mahallesi, Teknopark Bulvarı 1/3C, Kurtköy, 34906 Pendik/Istanbul
+      </p>
+      <p>Tel:+90 216 664 20 00</p>
+      <p>Fax:(0216) 664 20 00</p>
+      <p>Mail:info@intertech.com</p>
+      <p class="text-muted">© 2021</p>
+    </div>
+
+  
+
+    
+
+  
+
+    <div class="col-2 align-self-center text-center">
+    <img className='img-fluid footerLogo'
+                src={logo}
+                alt='icerik'
+                ></img>
+                <p className='text-center footerText'>INHERITIUM</p>
+    </div>
+  </footer>
+</div>
     </div>
   );
 }
