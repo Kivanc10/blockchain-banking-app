93f524989e5e80e93b4cbbfde7f0ddd405746ed8
akusgulu <akusgulu309@gmail.com>
2022-08-25 14:39:16 +0300

    Add routers for all pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
+import "antd/dist/antd.css";
 import "./components/css/App.css";
 import React from "react";
-import "antd/dist/antd.css";
-import Formm from "./components/formm";
-import Landing from "./components/landing";
-import Dashboard from "./components/dashboard";
-import AdminLogin from "./components/adminLogin";
-import Register from "./components/register";
-import GuestMain from "./components/GuestMain";
-import Logpage from "./components/logPage";
-import MyWallet from "./components/myWallet";
-import Transaction from "./components/Transaction";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import AdminPanel from "./components/adminPanel";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LogPage from "./components/logPage";
+import Dashboard from "./components/dashboard";
+import GuestMain from "./components/GuestMain"; // needs a lot of tweaking
+import Landing from "./components/landing";
+import LogPage from "./components/logPage"; // broken background
+import Register from "./components/register";
+import Transaction from "./components/Transaction";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Logpage />} />
         <Route path="/" element={<Landing />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/guestmain" element={<GuestMain />} />
+        <Route path="/logpage" element={<LogPage />} />
+        <Route path="/transaction" element={<Transaction />} />
+        <Route path="/adminpanel" element={<AdminPanel />} />
       </Routes>
     </div>
   );
