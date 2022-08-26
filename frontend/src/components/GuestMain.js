85c26ee0376656ab8a0bc13fb208904c65e0b0e1
ArdaSonmezler <64904720+ArdaSonmezler@users.noreply.github.com>
2022-08-26 15:59:14 +0300

    GuestMain

--- frontend/src/components/GuestMain.js
+++ frontend/src/components/GuestMain.js
@@ -2,6 +2,7 @@ import { Layout, Menu, Row } from "antd";
 import React from "react";
 import logo from "./raw/logoveyazi.png";
 import "./css/logPage.css";
+import Count from "./count";
 import Form from "react-bootstrap/Form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -54,19 +55,71 @@ const LogPage = () => (
     >
       <Content className="body">
         <Row style={{ marginTop: '10em' }}>
-          <div className="container-fluid col-8 d-flex justify-content-center">
+          <div className="container-fluid col-6 d-flex justify-content-center">
             <div className="container tRemain">
-              <h3 className="text-center">Days Remaining</h3>
+              
+              <h4 className="text-center">28/12/2027</h4>
+              <Count />
+              <Row>
+                <div className="container-fluid text-center d-flex justify-content-center">
+                  <p style={{fontSize:'0.7em',fontWeight:'Bold'}}>Years</p>
+                  <p style={{fontSize:'0.7em' , marginLeft:'2.6em',fontWeight:'Bold'}}>Minutes</p>
+                  <p style={{fontSize:'0.7em' , marginLeft:'2.6em',fontWeight:'Bold'}}>Days</p>
+                  <p style={{fontSize:'0.7em',fontWeight:'Bold', marginLeft:'2.6em',fontWeight:'Bold'}}>Hours</p>
+                  <p style={{fontSize:'0.7em' , marginLeft:'2.6em',fontWeight:'Bold'}}>Minutes</p>
+                  <p style={{fontSize:'0.7em' , marginLeft:'2.6em',fontWeight:'Bold'}}>Seconds</p>
+                </div>
+              </Row>
+              <h3 className="text-center">Remaining</h3>
             </div>
           </div>
         </Row>
         <Row style={{ marginTop: '1em' }}>
           <div className="container-fluid col-8 d-flex justify-content-between ">
-            <div className="BPbox">
-              <h3 className="text-center">Balance</h3>
+            <div style={{ height: "300px" }} className="BPbox">
+              <div style={{ marginTop: "50px" }} className="d-flex justify-content-evenly mb-3 portfolioFont">
+                Portfolio
+              </div>
+              <div className="d-flex justify-content-evenly mb-1 portfolioFont-2">
+                $2008.55
+              </div>
             </div>
-            <div className=" BPbox">
-              <h3 className="text-center">Parents</h3>
+            <div style={{ height: "300px" }} className=" BPbox">
+              <div className="mb-3 container-fluid col-8 d-flex TopHead">
+                <h3>Parents</h3>
+              </div>
+              <div className="container-fluid d-flex col-8 ">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">First</th>
+                      <th scope="col">Last</th>
+                      <th scope="col">Job</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Mark</td>
+                      <td>Zuckerberg</td>
+                      <td>Developer</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>Lebron</td>
+                      <td>James</td>
+                      <td>Basketball Player</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">3</th>
+                      <td>Charles</td>
+                      <td>Leclerc</td>
+                      <td>F1 Pilot</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
 
---
+++ frontend/src/components/count.js
@@ -0,0 +1,28 @@
+import { Col, Row, Statistic } from 'antd';
+import React from 'react';
+const { Countdown } = Statistic;
+const deadline = new Date('2023/10/27'); // Moment is also OK
+
+const App = () => {
+  const onFinish = () => {
+    console.log('finished!');
+  };
+
+  const onChange = (val) => {
+    if (4.95 * 1000 < val && val < 5 * 1000) {
+      console.log('changed!');
+    }
+  };
+  return (
+    <div className="App">
+        <Row>
+                <div className="container-fluid text-center d-flex justify-content-center">
+                <Countdown title="" value={deadline} format="YY MM DD HH mm ss"  className='countItems' style={{backgroundColor:'rgba(0,0,0,0.4)',width:'28em',borderRadius:'25px' }}/>
+
+                </div>
+        </Row>
+    
+    </div>
+  )
+};
+export default App;
