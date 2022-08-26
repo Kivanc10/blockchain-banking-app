21c9a6a2ae347c67d088b5c400e9ef1643b212f8
akusgulu <akusgulu309@gmail.com>
2022-08-26 11:29:19 +0300

    GuestMain template version

--- frontend/src/components/GuestMain.js
+++ frontend/src/components/GuestMain.js
@@ -2,129 +2,90 @@ import { Layout, Menu } from "antd";
 import React from "react";
 import logo from "./raw/logoveyazi.png";
 import "./css/dashboard.css";
-import "./css/GuestMain.css";
+// import "./css/GuestMain.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import * as V from "victory";
-import {
-  VictoryChart,
-  VictoryLine,
-  VictoryTheme,
-  VictoryPie,
-  VictoryBar,
-} from "victory";
+import intertech from "./raw/intertechLogo.png";
+import { MDBRow, MDBCol } from "mdb-react-ui-kit";
+
 import {
   faHome,
-  faWallet,
   faGlobe,
   faArrowRightFromBracket,
-  faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import "swiper/css";
 const { Header, Content, Footer, Sider } = Layout;
 
-const data = [
-  { Month: "January", earnings: 13000 },
-  { Month: "February", earnings: 17000 },
-  { Month: "March", earnings: 14250 },
-  { Month: "April", earnings: 15000 },
-  { Month: "May", earnings: 17000 },
-];
+const GuestMain = () => {
+  return (
+    <Layout hasSider>
+      <Sider
+        className="menu"
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <div className="logo" />
+        <Menu className="menu" defaultSelectedKeys={["4"]}>
+          <img
+            className="rounded mx-auto d-block mb-4 test"
+            src={logo}
+            alt="HeaderImage"
+          ></img>
+          <Menu.Item className="item mb-3">
+            <FontAwesomeIcon className="ServiceIcon" icon={faHome} /> Dashboard
+          </Menu.Item>
+          <Menu.Item className="item mb-3">
+            <FontAwesomeIcon
+              className="ServiceIcon"
+              icon={faArrowRightFromBracket}
+            />{" "}
+            Sign out
+          </Menu.Item>
+          <img
+            className="rounded mx-auto d-block fixed-bottom intertech2"
+            src={intertech}
+            alt="HeaderImage"
+          ></img>
+        </Menu>
+      </Sider>
 
-const sampleData = [
-  { x: "Guest1", earnings: 10 },
-  { x: "Guest2", earnings: 40 },
-];
-const dataBar = [
-  { x: 1, y: 2, y0: 1 },
-  { x: 4, y: 6, y0: 1 },
-  { x: 3, y: 7, y0: 1 },
-  { x: 2, y: 3, y0: 1 },
-];
+      <Layout
+        className="site-layout"
+        style={{
+          marginLeft: 200,
+        }}
+      >
+        <Content className="bodyDashboard justify-content-evenly">
+          <MDBRow style={{ marginTop: "150px", marginLeft: "220px" }}>
+            <MDBCol md="10 box5-2" className="d-flex justify-content-center">
+              <div style={{ height: "200px" }}>Days remaining</div>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow style={{ marginLeft: "250px", marginTop: "50px" }}>
+            <MDBCol md="4 box5-2" className="d-flex justify-content-evenly">
+              <div style={{ height: "300px" }}>Balance</div>
+            </MDBCol>
 
-const GuestMain = () => (
-  <Layout hasSider>
-    <Sider
-      className="menu"
-      style={{
-        overflow: "auto",
-        height: "100vh",
-        position: "fixed",
-        left: 0,
-        top: 0,
-        bottom: 0,
-      }}
-    >
-      <div className="logo" />
-      <Menu className="menu" defaultSelectedKeys={["4"]}>
-        <img className="img-fluid test" src={logo} alt="HeaderImage"></img>
-        <Menu.Item className="item">
-          <FontAwesomeIcon className="ServiceIcon" icon={faHome} /> Dashboard
-        </Menu.Item>
-        <Menu.Item className="item">
-          <FontAwesomeIcon className="ServiceIcon fa-light" icon={faWallet} />{" "}
-          My Wallet
-        </Menu.Item>
-        <Menu.Item className="item">
-          <FontAwesomeIcon className="ServiceIcon" icon={faGlobe} />{" "}
-          Transactions
-        </Menu.Item>
-        <Menu.Item className="item">
-          <FontAwesomeIcon
-            className="ServiceIcon"
-            icon={faArrowRightFromBracket}
-          />{" "}
-          Signout
-        </Menu.Item>
-      </Menu>
-    </Sider>
-    <Layout
-      className="site-layout"
-      style={{
-        marginLeft: 200,
-      }}
-    >
-      <Content className="body">
-        <div className="container-fluid d-flex justify-content-end header">
-          <button type="button" className="btn-primary">
-            <FontAwesomeIcon className="ServiceIcon fa-2x" icon={faUser} />
-          </button>
-        </div>
-
-        <div className="container-fluid">
-          <div className="col-12 Graph">
-            <VictoryChart theme={VictoryTheme.material} width={1000}>
-              <VictoryLine
-                height={500}
-                interpolation="natural"
-                style={{
-                  data: { stroke: "#c43a31" },
-                  parent: { border: "1px solid #ccc" },
-                }}
-                data={data}
-                // data accessor for x values
-                x="Month"
-                // data accessor for y values
-                y="earnings"
-              />
-            </VictoryChart>
-          </div>
-          <div className="col-12 Graph d-flex">
-            <VictoryPie
-              colorScale={["Blue", "orange"]}
-              labelRadius={({ innerRadius }) => innerRadius + 5}
-              data={[
-                { x: "Guest1", y: "%" + 50 },
-                { x: "Guest2", y: "%" + 50 },
-              ]}
-            />
-            <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
-              <VictoryBar style={{ data: { fill: "blue" } }} data={dataBar} />
-            </VictoryChart>
-          </div>
-        </div>
-      </Content>
+            <MDBCol
+              md="4 box5-2"
+              className="d-flex justify-content-evenly"
+              style={{ marginLeft: "150px" }}
+            >
+              <div>Parents</div>
+            </MDBCol>
+          </MDBRow>
+        </Content>
+      </Layout>
     </Layout>
-  </Layout>
-);
+  );
+
+  // zaman sayaci
+  // mirastan ne kadar para dusuyor
+};
 
 export default GuestMain;
