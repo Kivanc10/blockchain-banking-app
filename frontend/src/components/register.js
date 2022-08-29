97f51f53bf940ebfcf1ec5b9ac488ce1e2612559
akusgulu <akusgulu309@gmail.com>
2022-08-29 10:42:00 +0300

    Remove unnecessary fields from register

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -17,50 +17,39 @@ import { BankingContext } from "../context/BankingContext";
 import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-
 function Register() {
+  useEffect(() => {});
 
-  useEffect(() => {
-    
-  })
-
-  const[fNmae,setFname] = useState("")
-  const [lName,setLname] = useState("")
-  const [age,setAge] = useState(0)
-  const [fakeAgeStr,setFakeAgeStr] = useState("")
-  const [email,setEmail] = useState("")
-  const [phone,setPhone] = useState("")
-  const [id,setId] = useState("")
-
-
+  const [fNmae, setFname] = useState("");
+  const [lName, setLname] = useState("");
+  const [age, setAge] = useState(0);
+  const [fakeAgeStr, setFakeAgeStr] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [id, setId] = useState("");
 
   let navigate = useNavigate();
   const { currentAccount, addUser } = useContext(BankingContext);
 
-
-
   const onChange = (date, dateString) => {
     const birthDate = date._d.getFullYear();
-    const currentYear = new Date().getFullYear()
+    const currentYear = new Date().getFullYear();
     console.log(date);
     setAge(currentYear - birthDate);
-    console.log("age --> ", age)
-    
+    console.log("age --> ", age);
   };
 
-
   const addUserToSystem = async () => {
-    addUser(fNmae + lName,age,false)
-    .then((res) => {
-      //console.log(res)
-      navigate("/dashboard");
-      window.location.reload()
-    }).catch((e) => {
-      console.log("error ->", e.message)
-    })
-  }
-
+    addUser(fNmae + lName, age, false)
+      .then((res) => {
+        //console.log(res)
+        navigate("/dashboard");
+        window.location.reload();
+      })
+      .catch((e) => {
+        console.log("error ->", e.message);
+      });
+  };
 
   return (
     <div className="body">
@@ -81,68 +70,25 @@ function Register() {
             </div>
 
             <h3 className="mb-3" style={{ paddingLeft: "17%" }}>
-              Create a new guest account
+              What should we call you?
             </h3>
             <div className="mb-3">
-              <label>First name</label>
+              <label></label>
               <input
                 type="text"
                 className="form-control"
-                placeholder="Enter your first name"
-                onChange={e => setFname(e.target.value)}
-                value = {fNmae}
+                placeholder="Your name"
+                onChange={(e) => setFname(e.target.value)}
+                value={fNmae}
               />
             </div>
-            <div className="mb-3">
-              <label>Last name</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your last name"
-                onChange={e => setLname(e.target.value)}
-                value = {lName}
-              />
-            </div>
-            <div className="mb-3">
-              <label>Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Enter your email"
-                onChange={e => setEmail(e.target.value)}
-                value = {email}
-              />
-            </div>
-            <div className="mb-3">
-              <label>Phone number</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="(+123) 9876543210"
-                onChange={e => setPhone(e.target.value)}                
-                value = {phone}
-              />
-            </div>
-            <div className="mb-3">
-              <label>ID number</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your ID number"
-                onChange={e => setId(e.target.value)}
-                value = {id}
-              />
-            </div>
-            <div className="mb-3 date">
-              <label>Date of birth</label>
-              <div>
-                <Space direction="vertical">
-                  <DatePicker onChange={onChange} />
-                </Space>
-              </div>
-            </div>
+
             <div className="d-grid mb-2">
-              <button onClick={addUserToSystem} type="submit" className="btn btn-primary continue">
+              <button
+                onClick={addUserToSystem}
+                type="submit"
+                className="btn btn-primary continue"
+              >
                 Continue
               </button>
             </div>
@@ -158,9 +104,10 @@ function Register() {
       </div>
       <div>
         <img
-          className="img-fluid intertech"
+          className="img-fluid intertech fixed-bottom"
           src={intertech}
           alt="HeaderImage"
+          style={{ marginBottom: "50px" }}
         ></img>
       </div>
     </div>
